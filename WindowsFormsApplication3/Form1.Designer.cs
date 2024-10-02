5566c643335534a5b9917d3222924628a84244c9
warrantt <130606706+warrantt@users.noreply.github.com>
2024-10-02 12:42:37 +0300

    Proje dosyası ekle.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label8 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.timer6 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "solTaraf";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(689, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 40);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "SagTaraf";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Yellow;
+            this.label3.Location = new System.Drawing.Point(334, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 22);
+            this.label3.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(220, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(415, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(298, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(297, 22);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "60";
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(320, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "label8";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(114, 22);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 13);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(434, 22);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 13);
+            this.progressBar2.TabIndex = 6;
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Enabled = true;
+            this.timer5.Interval = 300;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // timer6
+            // 
+            this.timer6.Enabled = true;
+            this.timer6.Interval = 300;
+            this.timer6.Tick += new System.EventHandler(this.timer6_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(717, 324);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+        private System.Windows.Forms.Timer timer6;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        int doğmayeri=250;
+        Random doğmayerir = new Random();
+        int süre = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool g1 = false, g2 = false;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up) label2.Top -= 10;
+            if (e.KeyCode == Keys.Down) label2.Top += 10;
+
+            if (e.KeyCode == Keys.W) label1.Top -= 10;
+            if (e.KeyCode == Keys.S) label1.Top += 10;
+
+            if (e.KeyCode == Keys.NumPad0)
+            {
+                if (progressBar2.Value > 95)
+                {
+                    
+                    g1 = true;
+                }
+                else
+                {
+
+                }
+            }
+            if (e.KeyCode == Keys.D1)
+            {
+                if (progressBar1.Value > 95)
+                {
+                    
+                    g2 = true;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        Random yönbelirle = new Random();
+        int yön;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            yön = yönbelirle.Next(0, 6);
+        }
+        int p1 = 0, p2 = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (g1!=true&&g2!=true)
+            {
+                if (yön == 1)
+                {
+                    label3.Left -= 10;
+                    label3.Top -= 5;
+                }
+                else if (yön == 2)
+                {
+                    label3.Left -= 10;
+                    label3.Top += 5;
+                }
+                else if (yön == 3)
+                {
+                    label3.Left += 10;
+                    label3.Top -= 5;
+                }
+                else if (yön == 4)
+                {
+                    label3.Left += 10;
+                    label3.Top += 5;
+                }
+                else if (yön == 5)
+                {
+                    label3.Left += 10;
+                }
+                else if (yön == 0)
+                {
+                    label3.Left -= 10;
+                }
+            }
+
+            if (g1 == true)
+            {
+                if (timer4.Enabled != true)
+                {
+                    timer3.Enabled = true;
+                }
+            }
+
+            if (g2 == true)
+            {
+                if (timer3.Enabled != true)
+                {
+                    timer4.Enabled = true;
+                }               
+            }
+
+            if (yön == 1 || yön == 2 ||yön==0)
+            {
+                if ((label3.Top >= label1.Top - 20 && label3.Top <= label1.Top + 50) && (label3.Left <= label1.Left + 16))
+                {
+                    yön = yönbelirle.Next(3, 6);
+                    if (timer1.Interval > 2)
+                        timer1.Interval -= 2;
+                }
+            }
+            if (yön == 3 || yön == 4||yön==5)
+            {
+                if ((label3.Top >= label2.Top - 10 && label3.Top <= label2.Top + 50) && (label3.Left >= label2.Left - 16))
+                {
+                    yön = yönbelirle.Next(0, 3);
+                    if (timer1.Interval > 2)
+                        timer1.Interval -= 2;
+                }
+            }
+            if (yön == 1 || yön == 2 || yön ==0)
+            {
+                if (label3.Top < 0)
+                {
+                    yön = 2;
+                }
+                else if (label3.Top > 302)
+                {
+                    yön = 1;
+                }
+                if (label3.Left < 0)
+                {
+                    p2++;
+                    label5.Text = p2.ToString();
+                    doğmayeri = doğmayerir.Next(1, 300);
+                    label3.Left = 334;
+                    label3.Top = doğmayeri;
+                }
+            }
+            if (yön == 3 || yön == 4||yön==5)
+            {
+                if (label3.Top < 0)
+                {
+                    yön = 4;
+                }
+                else if (label3.Top > 302)
+                {
+                    yön = 3;
+                }
+
+                if (label3.Left > 690)
+                {
+                    p1++;
+                    label4.Text = p1.ToString();
+                    doğmayeri=doğmayerir.Next(1,300);
+                    label3.Left = 334;
+                    label3.Top = doğmayeri;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (süre > 0)
+                süre--;
+            label7.Text = süre.ToString();
+            if (süre < 8)
+                label7.ForeColor = Color.Red;
+
+            if (süre == 0)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                if (p1 < p2)
+                    MessageBox.Show("Kazanan Sağ taraf");
+                else if (p1 > p2)
+                    MessageBox.Show("Kazanan Sol Taraf");
+                else
+                    MessageBox.Show("Beraber");
+                DialogResult aa = new DialogResult();
+                aa=MessageBox.Show("Yeni Oyun?","Yeni Oyun",MessageBoxButtons.YesNo);
+                if (aa == DialogResult.Yes)
+                    Application.Restart();
+            }
+        }//süre
+        int say = 0;
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            say++;
+            progressBar2.Value = 0;
+            if (say < 200)
+            {
+                timer1.Enabled = false;
+                yön = 5;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                say = 0;
+                timer3.Enabled = false;
+                g1 = false;
+            }
+            if (label3.Left > 690)
+            {
+                p1++;
+                label4.Text = p1.ToString();
+                label3.Left = 334;
+            }
+
+
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            say++;
+            
+            if (say < 200)
+            {
+                timer1.Enabled = false;
+                yön = 0;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                say = 0;
+                timer4.Enabled = false;
+                g2 = false;
+            }
+            if (label3.Left < 0)
+            {
+                p2++;
+                label5.Text = p2.ToString();
+                label3.Left = 334;
+            }
+
+
+        }
+
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value < 100)
+                progressBar1.Value += 1;
+        }
+
+        private void timer6_Tick(object sender, EventArgs e)
+        {
+            if (progressBar2.Value < 100)
+                progressBar2.Value += 1;
+        }
+
+     
+
+       
+    }
+}
